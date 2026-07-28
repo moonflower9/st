d34575959e6f430ebfe5c9e4c093d01fc6e8a1dd
Connor Steel <csteel217@gmail.com>
2026-07-29 10:15:44 +1200

    update font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=21:antialias=true:autohint=true";
+static char *font = "NotoSansM Nerd Font Mono:pixelsize=17:antialias=true";
 static int borderpx = 2;
 
 /*
