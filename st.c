357b6a9d903a8ce90e6aa7e0be96222a3ca51c64
Connor Steel <csteel217@gmail.com>
2026-07-30 11:15:00 +1200

    patch boxdraw

--- st.c
+++ st.c
@@ -1213,6 +1213,9 @@ void tsetchar(Rune u, const Glyph *attr, int x, int y) {
   term.dirty[y] = 1;
   line[x] = *attr;
   line[x].u = u;
+
+  if (isboxdraw(u))
+    line[x].mode |= ATTR_BOXDRAW;
 }
 
 void tclearregion(int x1, int y1, int x2, int y2) {
